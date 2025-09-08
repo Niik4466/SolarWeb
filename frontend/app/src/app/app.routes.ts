4335760894ecab6ea3998f0ce3f4db528f65f285
ivan <107969090+free-hornet@users.noreply.github.com>
2025-09-08 11:36:42 -0300

    delete

--- frontend/app/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { GraficosComponent } from '../pages/graficos/graficos.component';
-
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'graficos', pathMatch: 'full' },
-  { path: 'graficos', component: GraficosComponent },
-  { path: '**', redirectTo: 'graficos' }
-];
