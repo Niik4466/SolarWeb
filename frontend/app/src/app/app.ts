e94f96377bbfd240d24bd1e8127d39cd329e6830
ivan <107969090+free-hornet@users.noreply.github.com>
2025-10-07 16:56:15 -0300

    Creación del footer + Logo de la Universidad

--- frontend/app/src/app/app.ts
+++ frontend/app/src/app/app.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/ui/navbar/navbar.component';
+import { Footer } from './components/ui/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   //Se agregan los imports necesarios
-  imports: [CommonModule, RouterOutlet, NavbarComponent], 
+  imports: [CommonModule, RouterOutlet, NavbarComponent, Footer], 
 
   //Template agrega el componente necesario
   template: `
@@ -15,6 +16,7 @@ import { NavbarComponent } from './components/ui/navbar/navbar.component';
     <main class="container">
       <router-outlet></router-outlet>
     </main>
+    <app-footer></app-footer>
   `,
   styles: [`.container{max-width:1200px;margin:0 auto;padding:1rem}`]
 })
