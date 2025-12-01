374113a7343631ecb1b1c949b34ceaee61795d6f
lombrices <claudia.vergara01@alumnos.uach.cl>
2025-12-01 11:00:36 -0300

    Corrigiendo rutas de redireccionamiento en login

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -39,7 +39,7 @@ export const routes: Routes = [
   // ---------------------------
   {
     path: '**',
-    component: Inicio,
+    component: GraficosComponent,
     canActivate: [redirectGuard],
   },
 ];
--- frontend/app/src/app/pages/auth/login/login.ts
+++ frontend/app/src/app/pages/auth/login/login.ts
@@ -68,7 +68,7 @@ export class LoginComponent {
           // Caso: login exitoso y usuario APROBADO
           if (res.success && res.estado === 'aprobado') {
             const returnUrl =
-              this.route.snapshot.queryParamMap.get('returnUrl') || '/inicio';
+              this.route.snapshot.queryParamMap.get('returnUrl') || '**';
             this.router.navigateByUrl(returnUrl);
             return;
           }
