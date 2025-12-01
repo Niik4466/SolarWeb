3dbb496a5bc374334b3305f3108175395d3222f8
lombrices <claudia.vergara01@alumnos.uach.cl>
2025-12-01 10:34:57 -0300

    Arreglando imagen de fondo

--- frontend/app/src/app/pages/auth/login/login.ts
+++ frontend/app/src/app/pages/auth/login/login.ts
@@ -1,5 +1,5 @@
 // src/app/pages/auth/login/login.ts
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
@@ -41,7 +41,7 @@ export class LoginComponent {
    */
   loading = signal(false);
   errorMsg = signal<string | null>(null);
-
+  
   /**
    * Handler del submit del formulario de login.
    */
@@ -87,5 +87,12 @@ export class LoginComponent {
           this.errorMsg.set(err?.error?.detail ?? 'Error al iniciar sesión.');
         },
       });
+  }
+  ngOnInit(): void {
+    document.body.classList.add('login-bg');
+  }
+
+  ngOnDestroy(): void {
+    document.body.classList.remove('login-bg');
   }
 }
